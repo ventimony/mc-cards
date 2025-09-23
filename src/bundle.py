151fcfee0cff31bab17212bead9cac74371942f9
ventimony <inspace5137@gmail.com>
2025-09-24 00:33:51 +0800

    feat: v0.2.1 (#7)
    * feat: update pack for Minecraft 1.21.8
    * feat: update models, functions, recipe, textures
    * feat: use large models in certain contexts

--- src/bundle.py
+++ src/bundle.py
@@ -1,11 +1,20 @@
 from argparse import ArgumentParser
 import os
+import json
 
-DIR = "PlayingCards_datapack/data/cards/function"
+ROOT = "PlayingCards_datapack/data/cards"
+DIR = f"{ROOT}/function"
 FILE = "deck"
+SUITS = ["diamond", "club", "heart", "spade"]
 
+def get_cards():
+  cards = [(suit, idx) for suit in SUITS for idx in range(1,14)]
+  cards += [(card, 0) for card in ["blank", "joker_red", "joker_black", "back"]]
 
-def build(directory: str, file: str, start: int = 52000):
+  idxs = list(range(1, 53)) + [0, 53, 54, 55]
+  return cards, idxs
+
+def build(directory: str, file: str, start: int = 52000, version: str = "0.2"):
   """Generates a mcfunction file that gives the player a custom filled bundle
 
   Args:
@@ -14,18 +23,126 @@ def build(directory: str, file: str, start: int = 52000):
       start (int, optional): Starting index for model data. Defaults to 52000.
   """
   os.makedirs(directory, exist_ok=True)
-
-  idxs = list(range(1, 53)) + [0, 53, 54, 55]
-  items = ", ".join([
-    f"""{{"id": "minecraft:paper", "count": 1, "components": {{"minecraft:custom_model_data": {start + idx}, "minecraft:item_name": '{{"translate":"card.{idx}"}}'}}}}""" for idx in idxs
-  ])
-
-  func = f"give @s minecraft:bundle[minecraft:bundle_contents=[{items}]] 1" 
+  func = gen_func(version)
   
   with open(os.path.join(directory, f"{file}.mcfunction"), 'w') as f:
     f.write(func)
 
   print(f"Generated function `{file}` in {directory} with {start=}")
+
+def get_model_data(card: tuple[str, int, int], start: int = 52000, version: str = "0.2"):
+  match version:
+    case "0.2":
+      (suit, value, _) = card
+      match suit:
+        case _ if suit in SUITS:
+          data = f"""{{"strings": ["cards","{suit}.{value:02}"]}}"""
+        case _:
+          data = f"""{{"strings": ["cards","{suit}"]}}"""
+    case _:
+      (_, _, idx) = card
+      data = f"{start + idx}"
+
+  return data
+
+def get_model_data_recipe(card: tuple[str, int, int], start: int = 52000, version: str = "0.2"):
+  match version:
+    case "0.2":
+      (suit, value, _) = card
+      match suit:
+        case _ if suit in SUITS:
+          data = {"strings": ["cards",f"{suit}.{value:02}"]}
+        case _:
+          data = {"strings": ["cards", suit]}
+    case _:
+      (_, _, idx) = card
+      data = f"{start + idx}"
+
+  return data
+
+
+def get_item_name(card: tuple[str, int, int], version: str = "0.2"):
+  match version:
+    case _:
+      (_, _, idx) = card
+      data = f"card.{idx}"
+
+  return data
+
+def get_equippable():
+  return f"""{{"slot": "head"}}"""
+
+def get_components(card: tuple[str, int, int], version: str = "0.2"):
+  match version:
+    case "0.2":
+      data = f"""{{"minecraft:custom_model_data": {get_model_data(card, version)}, "minecraft:item_name": {{"translate":"{get_item_name(card, version)}"}}, "minecraft:equippable": {get_equippable()}}}"""
+    case _:
+      data = f"""{{"minecraft:custom_model_data": {get_model_data(card, version)}, "minecraft:item_name": '{{"translate":"{get_item_name(card, version)}"}}'}}"""
+  
+  return data
+
+def gen_items(cards: list[tuple[str, int, int]], version: str = "0.2"):
+  return ", \ \n".join([
+    f"""{{"id": "minecraft:paper", "count": 1, "components": {get_components(card, version)}}}""" for card in cards
+  ])
+
+def gen_func(version: str = "0.2"):
+  """Generates the function string for MC versions after 1.21.4
+  Args:
+      start (int): Starting index for model data.
+  """
+  idxs = list(range(1, 53)) + [0, 53, 54, 55]
+
+  cards, idxs = get_cards()
+  cards = [(suit, val, idx) for (suit, val), idx in zip(cards, idxs)]
+
+  return f"give @s minecraft:bundle[minecraft:bundle_contents=[{gen_items(cards, version)}]] 1" 
+
+def gen_contents(cards: list[tuple[str, int, int]], version: str = "0.2"):
+  return [
+    {
+      "id": "minecraft:paper", 
+      "count": 1, 
+      "components": {
+        "minecraft:custom_model_data": get_model_data_recipe(card, version),
+        "minecraft:item_name": {
+          "translate": get_item_name(card, version)
+        },
+        "minecraft:equippable": {"slot": "head"}
+      }
+    }
+    for card in cards
+  ]
+
+def get_ingredients(version: str):
+  match version:
+    case "0.2":
+      ingredients = ["minecraft:paper", "minecraft:red_dye", "minecraft:bundle", "minecraft:black_dye"]
+    case _:
+      ingredients = [{"item": "minecraft:paper"}, {"item": "minecraft:red_dye"}, {"item": "minecraft:bundle"}, {"item": "minecraft:black_dye"}]
+
+  return ingredients
+
+def gen_recipe(out: str, name: str, version: str = "0.2"):
+  cards, idxs = get_cards()
+  cards = [(suit, val, idx) for (suit, val), idx in zip(cards, idxs)]
+  recipe = {
+    "type": "minecraft:crafting_shapeless",
+    "ingredients": get_ingredients(version),
+    "result": {
+        "id": "minecraft:bundle",
+        "count": 1,
+        "components": {
+            "bundle_contents": gen_contents(cards, version)
+        }
+    }
+  }
+  os.makedirs(out, exist_ok=True)
+  
+  with open(os.path.join(out, f"{name}.json"), 'w') as json_file:
+    json.dump(recipe, json_file, indent=2)
+
+  print(f"Generated recipe `{name}` in {out}")
 
 if __name__ == "__main__":
   parser = ArgumentParser()
@@ -35,3 +152,4 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   build(args.out, args.file, args.start)
+  gen_recipe(f"{ROOT}/recipe", "deck")
--- src/models.py
+++ src/models.py
@@ -1,27 +1,245 @@
 import json
 import os
 
-DIR = "PlayingCards_v0.1.0/assets/cards/models/item"
+ROOT = "PlayingCards/assets"
+DIR = f"{ROOT}/cards/models/item"
 
-if __name__ == "__main__":
-  os.makedirs(DIR, exist_ok=True)
+SUITS = ["diamond", "club", "heart", "spade"]
+RED = ["diamond", "heart"]
+
+def gen_item(out: str, name: str, textures: dict[str, str], namespace: str, type: str = "item", parent: str = "base", ext: str = "json"):
+  """Generate an item model that derives from a parent model in a given namespace.
+
+  Args:
+    out (str): The output directory for the model file
+    name (str): Name of the model file
+    textures (dict[str, str]): Texture key mapping
+    namespace (str): Namespace for the model. e.g. 'minecraft'
+    type (str, optional): The model type. e.g. 'block' or 'item'. Defaults to "item".
+    parent (str, optional): Name of the parent model. Defaults to "base".
+    ext (str, optional): The output file extension. Defaults to "json".
+  """
+
+  prefix = f"{namespace}:{type}"
+
+  # Create the data for the JSON file
+  data = {
+    "parent": f"{prefix}/{parent}",
+    "textures": textures
+  }
+  
+  # Define the filename
+  filename = f"{name}.{ext}"
+  
+  # Write the JSON file
+  with open(os.path.join(out, filename), 'w') as json_file:
+    json.dump(data, json_file, indent=2)
+
+
+def gen_items(out: str, items: list[tuple[str, dict[str, str]]], namespace: str = "cards", type: str = "item", parent: str = "base", ext: str = "json"):
+  """Generate the item model files
+
+  Args:
+    out (str): path of the models/items directory
+  """
+  os.makedirs(out, exist_ok=True)
 
   # Generate JSON files
-  for suit in "dchs":
-      for idx in range(1,14):
-          # Create the data for the JSON file
-          data = {
-              "parent": "cards:item/base",
-              "textures": {
-                  "0": f"cards:item/{suit}_{idx:02}"
-              }
-          }
-          
-          # Define the filename
-          filename = f"{suit}_{idx:02}.json"
-          
-          # Write the JSON file
-          with open(os.path.join(DIR, filename), 'w') as json_file:
-            json.dump(data, json_file, indent=2)
+  for (name, textures) in items:
+    gen_item(out, name, textures, namespace, type, parent, ext)
+  
+  print("Generated item models files.")
 
-  print("Generated JSON files.")
+def get_textures(suit: str, value: int, version: str = "0.1"):
+  match version:
+    case "0.2" | "0.2.1":
+      match suit:
+        case _ if suit in SUITS:
+          colour = "r" if suit in RED else "b"
+
+          textures = {
+            "value": f"cards:item/value/{colour}_{value:02}",
+            "suit": f"cards:item/suit/{suit}"
+            }
+        case "blank":
+          textures = {
+            "value": "cards:item/empty",
+            "suit": "cards:item/empty"
+            }
+        case _:
+          textures = {
+            "value": "cards:item/empty",
+            "suit": f"cards:item/{suit}"
+            }
+          
+    case _:
+      textures = {"0": f"cards:item/{suit}_{value:02}"}
+  
+  return textures
+
+def gen_model_case(suit: str, value: int, prefix: str, version: str = "0.2"):
+  if suit in SUITS:
+    model = f"{prefix}/{suit}/{suit}_{value:02}"
+    when = f"{suit}.{value:02}"
+  else:
+    model = f"{prefix}/{suit}"
+    when = f"{suit}"
+  
+  match version:
+    case "0.2.1":
+      # display contexts
+      # first-person: large
+      # third: base + blank
+      # head: base (+ band?)
+      # gui: base
+      # ground: base
+      # fixed: large
+
+      model = {
+        "model": {
+            "type": "minecraft:select",
+            "property": "minecraft:display_context",
+            "fallback": { # base
+              "type": "minecraft:model",
+              "model": model
+            },
+            "cases": gen_display_contexts(model, prefix, version)
+          },
+        "when": when
+      }
+
+      return model
+
+  return {
+    "model": {
+        "type": "minecraft:model",
+        "model": model
+      },
+    "when": when
+  }
+
+def gen_display_contexts(model: str, prefix: str, version: str = "0.2"):
+  return [gen_display_context(context, model, prefix, version) for context in [
+    "blank", # thirdperson
+    "large", # firstperson, fixed
+    ]]
+
+def gen_display_context(context: str, model: str, prefix: str, version: str = "0.2"):
+  match context:
+    case "blank":
+      ctx = ["thirdperson_lefthand", "thirdperson_righthand"]
+      ctx_model = {
+        "type": "minecraft:model",
+        "model": f"{prefix}/blank"
+      }
+    case "large":
+      ctx = ["firstperson_lefthand", "firstperson_righthand", "fixed"]
+      ctx_model = {
+        "type": "minecraft:model",
+        "model": f"{model}_l"
+      }    
+    case _:
+      ctx = context
+      ctx_model = {
+        "type": "minecraft:model",
+        "model": model
+      } 
+
+  return {
+    "model": ctx_model,
+    "when": ctx
+  }
+
+def gen_model_cases(prefix: str, cards: list[tuple[str, int, str]], version: str = "0.2"):
+  return [gen_model_case(suit, card, prefix, version) for (suit, card, _) in cards]
+
+def gen_model(assets: str, name: str, cards: list[tuple[str, int, str]], version: str = "0.1", start: int = 52000, filter: str | list[str] = "cards"):
+  """Generate the model file that overrides the vanilla model
+  
+  Pre 1.21.4: <namespace>/models/item
+  1.21.4+: <namespace>/items
+  """
+  filename = f"{name}.json"
+  prefix = "cards:item"
+  
+  match version:
+    case "0.2" | "0.2.1":
+      out = "minecraft/items"
+
+      model = {
+        "model": {
+          "type": "minecraft:select",
+          "property": "minecraft:custom_model_data",
+          "fallback": {
+            "type": "minecraft:model",
+            "model": f"minecraft:item/{name}"
+          },
+          "cases": [{
+            "model": {
+              "type": "minecraft:select",
+              "property": "minecraft:custom_model_data",
+              "index": 1,
+              "fallback": {
+                "type": "minecraft:model",
+                "model": f"{prefix}/blank"
+              },
+              "cases": gen_model_cases(prefix, cards, version),
+            },
+            "when": filter
+          }]
+        }
+      }
+          
+    case _:
+      out = "minecraft/models/item"
+      
+      names = ["base"] + \
+              [f"{suit}_{idx:02}" for suit in "dchs" for idx in range(1,14)] + \
+              ["joker_red", "joker_black", "back"]
+
+      model = {
+        "parent": "minecraft:item/generated",
+        "textures": {
+          "layer0": f"minecraft:item/{name}"
+        },
+        "overrides": [
+          { 
+            "predicate": {"custom_model_data": start + idx}, 
+            "model": f"cards:item/{card}"
+            }
+          for (idx, card) in enumerate(names)
+        ]
+      }
+  
+  os.makedirs(f"{assets}/{out}", exist_ok=True)
+
+  # Write the JSON file
+  with open(os.path.join(assets, out, filename), 'w') as json_file:
+    json.dump(model, json_file, indent=2)
+
+  print("Generated overriding model.")
+
+def gen_cards(out: str, version: str):
+  cards = ["blank", "joker_red", "joker_black", "back"]
+  cards = [(card, 0, card) for card in cards]
+
+  match version:
+    case "0.2" | "0.2.1":
+      for suit in SUITS:
+        os.makedirs(f"{out}/{suit}", exist_ok=True)
+      
+      cards += [(suit, idx, f"{suit}/{suit}_{idx:02}") for suit in SUITS for idx in range(1,14)]
+    
+    case _:
+      cards += [(suit, idx, f"{suit}_{idx:02}") for suit in "dchs" for idx in range(1,14)]
+  
+  
+  items = [(name, get_textures(suit, card, version)) for (suit, card, name) in cards]
+  gen_items(out, items)
+
+  items_large = [(f"{name}_l", get_textures(suit, card, version)) for (suit, card, name) in cards]
+  gen_items(out, items_large, parent="large")
+  gen_model(ROOT, "paper", cards, version)
+
+if __name__ == "__main__":
+  gen_cards(DIR, "0.2.1")
